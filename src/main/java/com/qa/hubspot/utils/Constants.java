d5c4941c333a0003173291450df2dbe7b9eea760
rahulrajautomation <rahul.raj4778@gmail.com>
2020-06-10 15:40:35 +0530

    Login page fixed

--- src/main/java/com/qa/hubspot/utils/Constants.java
+++ src/main/java/com/qa/hubspot/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	//*****************************Constants Name*******************************
 
 
-	public static final String LOGIN_PAGE_TITLE="HubSpot Login12";
+	public static final String LOGIN_PAGE_TITLE="HubSpot Login";
 	public static final String HOME_PAGE_TITLE="Reports dashboard";
 	public static final String HOME_PAGE_HEADER="Sales Dashboard";
 	public static final String CONTACTS_PAGE_TITLE="Contacts";
